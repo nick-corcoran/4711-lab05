a88fcf6e787ebcead2d77b5e23d5be6aa1306802
Nick Corcoran <nicholascorcoran490@gmail.com>
2020-02-26 21:21:31 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var app = express();
+var bodyParser  = require("body-parser");
+var path = require("path");
+var fs = require("fs")
+
+app.use(express.static(path.join(__dirname, '/public/css')));
+app.use(express.static(path.join(__dirname, '/public/js')));
+app.use(bodyParser.json());
+
+
+const expressHbs = require('express-handlebars');
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.listen(3000, () => console.log("connected"));
+
+ 
+//called when request for page is made
+app.get("/", (req, res) => {
+    fs.readFile('./public/jsonData.json', (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        else {
+            var array = JSON.parse(data)
+            res.render('artist', { "artist": array })
+        }
+    })
+
+})
+
+// called when request is made to add a new artist
+app.post("/new", (req, res) => {
+    fs.readFile('./public/jsonData.json', (err, data) => {
+        if (err) {
+            return console.error(err);
+        }
+        else {
+            var dataToInsert = req.body;
+            var array = JSON.parse(data);
+            console.log(dataToInsert);
+            array.push(dataToInsert);
+            var suc = writeToFile(array);
+            if (suc){
+                sendDataToClient(res)
+            }
+            
+        }
+    })
+    
+})
+
+//called when a request is made to delete an artist
+app.get("/delete", (req, res) => {
+    var id = req.query.id;
+    fs.readFile("./public/jsonData.json", (err, data) => {
+        if (err) {
+            console.error("error");
+        }
+        else {
+            var array = JSON.parse(data);
+            array.splice(id, 1);
+            var success = writeToFile(array)
+            if (success) {
+                console.log("leedle")
+                sendDataToClient(res);
+            }
+        }
+    })
+})
+
+
+function sendDataToClient(res) {
+    fs.readFile("./public/jsonData.json", (err,data) => {
+        if (err) {
+            console.log("error")
+        }
+        else {
+            var array = JSON.parse(data);
+            res.json(array);
+        }
+    })
+}
+
+function writeToFile(data) {
+    fs.writeFile("./public/jsonData.json", JSON.stringify(data), (err) => {
+        if (err) {
+            console.error(err)
+            return next(err);
+        }
+    
+    })
+    console.log("file write success");
+    return true;
+    
+}
---
+++ public/js/script.js
@@ -0,0 +1,136 @@
+
+
+function clearArtistFields() {
+    var name = document.getElementById("AName").value = "";
+    var about = document.getElementById("AAbout").value = "";
+    var image = document.getElementById("AImage").value = "";
+
+}
+
+function displayArtistFields() {
+    var obj = document.getElementById("aFields")
+    var obj2 = document.getElementById("ArtistBox");
+    if (obj.style.display == "flex") {
+        obj.style.display = "none";
+        obj2.style.display = "flex";
+        clearArtistFields();
+    }
+    else {
+        obj.style.display = "flex";
+        obj2.style.display = "none"
+    }
+
+}
+
+function submitFields() {
+    var name = document.getElementById("AName").value;
+    var about = document.getElementById("AAbout").value;
+    var image = document.getElementById("AImage").value;
+    if (name.length == 0 || about.length == 0 || image.length == 0) {
+        alert("Please Make Sure all fields have input")
+    }
+    else if (about.length > 40) {
+        alert("about cannot be greater than 40")
+    }
+    else {
+        addNewArtistToServer(name, about, image)
+        clearArtistFields();
+        document.getElementById("aFields").style.display = "none";
+    }
+
+
+}
+
+function deleteArtist(id) {
+    console.log(id);
+    let url = new URL("http://localhost:3000/delete");
+    let params = { "id": id }
+    url.search = new URLSearchParams(params).toString();
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var array = data;
+            console.log(data);
+            document.getElementById("ArtistBox").innerHTML = "";
+            for (var k = 0; k < array.length; k++) {
+                console.log("inside")
+                addArtist(array[k].name, array[k].description, array[k].image, k);
+            }
+        })
+        .catch(err => console.log("error reading"))
+
+}
+
+
+
+function addNewArtistToServer(name, about, image, count) {
+    var artist = { "name": name, "description": about, "image": image, "id": count }
+    fetch("http://localhost:3000/new",
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(artist)
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("ArtistBox").innerHTML = "";
+            var array = data;
+            for (var k = 0; k < array.length; k++) {
+                console.log("inside")
+                addArtist(array[k].name, array[k].description, array[k].image, k);
+            }
+        })
+        .catch(err => console.log("error reading"))
+}
+
+
+function search() {
+    var searchText = document.getElementById("DirInput").value.toLowerCase();
+    console.log("search: " + searchText);
+    var array = document.getElementsByTagName("h3");
+    for (var k = 0; k < array.length; k++) {
+        var word = array[k].textContent.toLowerCase();
+        console.log(word);
+        if (!(word.includes(searchText))) {
+            document.getElementById(k).parentNode.parentNode.style.display = "none";
+        }
+        else if (searchText.includes(searchText)) {
+            document.getElementById(k).parentNode.parentNode.style.display = "flex";
+        }
+    }
+    document.getElementById("DirInput").value = "";
+}
+
+
+
+function addArtist(name, about, imageURL, count) {
+
+    var artistContainer = document.getElementById("ArtistBox");
+    var artist = document.createElement("div");
+    artist.classList = "Artist";
+    var image = document.createElement("img");
+    image.src = imageURL;
+    var infoDiv = document.createElement("div");
+    var h3 = document.createElement("h3");
+    h3.textContent = name;
+    h3.id = "h" + count;
+    var ptag = document.createElement("p");
+    ptag.textContent = about;
+    var bDiv = document.createElement("div");
+    var button = document.createElement("button");
+    button.classList = "delete";
+    button.id = count;
+    button.textContent = "delete";
+    button.addEventListener("click", function () { deleteArtist(button.id) });
+    artistContainer.appendChild(artist);
+    artist.appendChild(image);
+    artist.appendChild(infoDiv);
+    artist.appendChild(bDiv);
+    infoDiv.appendChild(h3);
+    infoDiv.appendChild(ptag);
+    bDiv.appendChild(button);
+    artistContainer.style.display = "flex";
+}
+
+
+
